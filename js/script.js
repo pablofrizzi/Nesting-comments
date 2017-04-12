c3dcb9adf9f1ab04d83c26590be02caacc528afa
pablofrizzi <pablofrizzi@gmail.com>
2017-04-12 10:21:56 +0200

    elements made in jquery

--- js/script.js
+++ js/script.js
@@ -2,51 +2,82 @@ $(document).ready(main);
 
 function main() {
     
-   $('.comment-btn').one('click', showCommentForm); 
-   commentContainer.on('click', '.publish-btn', renderComment);
-    
+   article.on('click', '.comment-btn', showCommentForm); 
     
 }
 
+var article = $('.article');
 var commentContainer = $('.comment-container');
 
 
 function showCommentForm() {
-    var commentForm = $(commentFormTemplate);
+    
+    var commentFormDiv = $('<div>').addClass('comment-form form-group');
+    var commentFormNameLabel = $('<label>').text('Your name:');   
+    var commentFormInput = $('<input>').addClass('name form-control');
+    var commentFormTextLabel = $('<label>').text('Comment:');
+    var commentFormTextArea = $('<textarea>').addClass('comment form-control');
+    var commentFormPublishBtn = $('<button>').text('Publish')
+                                             .addClass('btn btn-default publish-btn');
+    
+    
+    commentFormDiv.append(commentFormNameLabel);
+    commentFormDiv.append(commentFormInput);
+    commentFormDiv.append(commentFormTextLabel);
+    commentFormDiv.append(commentFormTextArea);
+    commentFormDiv.append(commentFormPublishBtn);
+    
 
-    commentForm.appendTo('.article');
-    commentForm.fadeIn();
+    commentFormDiv.appendTo('.article');
+    commentFormDiv.fadeIn();
+
+
+    $('.publish-btn').on('click', renderComment);
+    
+    
     
 }
 
 function renderComment() {
-    var comment = $(commentTemplate);
-    var commentForm = $(commentFormTemplate);
-    var commentContainer = $('.comment-container');
     
-    comment.find('.user-name').text(commentForm.find('.name').val() + ' writes:');
-    comment.find('.user-comment').text(commentForm.find('.comment').val());
-    comment.appendTo(commentContainer);
-    comment.fadeIn();
+    var commentDiv = $('<div>').addClass('comment-publish');
+    var commentHeader = $('<h2>').addClass('user-name');
+    var commentParagraph = $('<p>').addClass('user-comment');
+    var commentBtn = $('<button>').text('Comment')
+                                  .addClass('btn btn-default comment-btn');
+    
+    commentDiv.append(commentHeader);
+    commentDiv.append(commentParagraph);
+    commentDiv.append(commentBtn);
+    
+    
+    var commentContainer = $('.comment-container');
+    var commentForm = $('.comment-form');
+    
+    commentDiv.find('.user-name').text(commentForm.find('.name').val() + ' writes:');
+    commentDiv.find('.user-comment').text(commentForm.find('.comment').val());
+    commentDiv.appendTo(commentContainer);
+    commentDiv.fadeIn();
     
     commentForm.fadeOut();
     
+    
 }
 
-var commentFormTemplate = `
-    <div class="comment-form form-group">
-        <label for="name">Your name:</label>
-        <input class="name form-control">
-        <label for="comment">Comment:</label>
-        <textarea class="comment form-control"></textarea>
-        <button type="button" class="btn btn-default publish-btn">Publish</button>
-    </div>
-`;
-
-var commentTemplate = `
-    <div class="comment-publish">
-        <h2 class="user-name"></h2>
-        <p class="user-comment"></p>
-        <button type="button" class="btn btn-default comment-btn">Comment</button>
-    </div>
-`;
+//var commentFormTemplate = `
+//    <div class="comment-form form-group">
+//        <label for="name">Your name:</label>
+//        <input class="name form-control">
+//        <label for="comment">Comment:</label>
+//        <textarea class="comment form-control"></textarea>
+//        <button type="button" class="btn btn-default publish-btn">Publish</button>
+//    </div>
+//`;
+//
+//var commentTemplate = `
+//    <div class="comment-publish">
+//        <h2 class="user-name"></h2>
+//        <p class="user-comment"></p>
+//        <button type="button" class="btn btn-default comment-btn">Comment</button>
+//    </div>
+//`;
